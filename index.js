fa76caae910a92c870cf869337c275c367838522
jahed121421 <nabilahmed121421@gmail.com>
2023-05-21 15:32:05 +0600

    data modify in index file

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
-// middleware
-const corsConfig ={
-  origin:'',
-  credential: true,
-  methods: ["GET", "PUT", "PATCH", "DELETE", "OPTIONS", "POST"]
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -28,6 +22,87 @@ const client = new MongoClient(uri, {
   }
 });
 
+const database = client.db("toyshop").collection('alldata');
+
+
+const indexKeys = {name:1, catagory:1};
+const indexOptions = {titel: "nameCategory"};
+const result = await database.createIndex(indexKeys, indexOptions);
+
+
+
+app.get("/datasearch/:text", async(req, res)=>{
+  const searchText = req.params.text;
+  const result = await database.find({
+    $or :[{
+      name: {$regex: searchText, $options: "i"}
+    },
+    {
+      catagory: {$regex: searchText, $options: "i"}
+    }],
+  }).toArray();
+  res.send(result);
+})
+
+app.get('/alldata', async(req, res)=>{
+  const cousor = database.find();
+  const result = await cousor.limit(20).toArray();
+  res.send(result);
+})
+
+app.post('/sendtoydata', async(req, res)=>{
+  const body = req.body;
+  console.log(body);
+  const result = await database.insertOne(body);
+  res.send(result);
+})
+
+app.get('/alldata/:id', async(req,res)=>{
+const id = req.params.id;
+const query = {_id : new ObjectId(id)}
+const result = await database.findOne(query);
+res.send(result)
+})
+app.get('/mytoy', async(req,res)=>{
+  let query = {};
+  if(req.query?.email){
+    query = {email: req.query.email
+    }
+  }
+const result = await database.find(query).toArray();
+res.send(result)
+})
+app.delete('/mytoy/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await database.deleteOne(query);
+  res.send(query);
+})
+
+app.get('/mytoy/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await database.findOne(query);
+  res.send(result)
+})
+app.put('/mytoy/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const body = req.body;
+  const update ={
+    $set: {
+      price :body.price,
+      description: body.description,
+      quantity: body.quantity,
+      image: body.image
+
+    },
+  };
+  const result = await database.updateOne(query, update);
+  res.send(result)
+});
+
+
 async function run() {
   try {
 
@@ -36,86 +111,6 @@ async function run() {
     //  await client.connect();
 
 
-
-    const database = client.db("toyshop").collection('alldata');
-
-
-    const indexKeys = {name:1, catagory:1};
-    const indexOptions = {titel: "nameCategory"};
-    // const result = await database.createIndex(indexKeys, indexOptions);
-
-
-
-    // app.get("/datasearch/:text", async(req, res)=>{
-    //   const searchText = req.params.text;
-    //   const result = await database.find({
-    //     $or :[{
-    //       name: {$regex: searchText, $options: "i"}
-    //     },
-    //     {
-    //       catagory: {$regex: searchText, $options: "i"}
-    //     }],
-    //   }).toArray();
-    //   res.send(result);
-    // })
-    
-    app.get('/alldata', async(req, res)=>{
-      const cousor = database.find();
-      const result = await cousor.limit(20).toArray();
-      res.send(result);
-    })
-    
-    app.post('/sendtoydata', async(req, res)=>{
-      const body = req.body;
-      console.log(body);
-      const result = await database.insertOne(body);
-      res.send(result);
-    })
-
-    app.get('/alldata/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id : new ObjectId(id)}
-    const result = await database.findOne(query);
-    res.send(result)
-    })
-    app.get('/mytoy', async(req,res)=>{
-      let query = {};
-      if(req.query?.email){
-        query = {email: req.query.email
-        }
-      }
-    const result = await database.find(query).toArray();
-    res.send(result)
-    })
-    app.delete('/mytoy/:id', async (req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await database.deleteOne(query);
-      res.send(query);
-    })
-
-    app.get('/mytoy/:id', async (req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await database.findOne(query);
-      res.send(result)
-    })
-    app.put('/mytoy/:id', async (req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const body = req.body;
-      const update ={
-        $set: {
-          price :body.price,
-          description: body.description,
-          quantity: body.quantity,
-          image: body.image
-
-        },
-      };
-      const result = await database.updateOne(query, update);
-      res.send(result)
-    });
 
    
     
